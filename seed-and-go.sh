42934fe8a775747f71673b0999adab54a258c2e2
mathew-fleisch <mathew.fleisch@gmail.com>
2020-12-11 19:51:30 -0800

    Explicitly set env vars in .env file if it doesn't exist

--- seed-and-go.sh
+++ seed-and-go.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
-
+if ! [[ -f .env ]]; then 
+    touch .env
+    if [[ -n "$REDIS_HOST" ]]; then
+        echo "export REDIS_HOST=$REDIS_HOST" >> .env
+    fi
+    if [[ -n "$REDIS_POST" ]]; then
+        echo "export REDIS_POST=$REDIS_POST" >> .env
+    fi
+    if [[ -n "$REDIS_PASSWORD" ]]; then
+        echo "export REDIS_PASSWORD=$REDIS_PASSWORD" >> .env
+    fi
+    if [[ -n "$REDIS_KEY" ]]; then
+        echo "export REDIS_KEY=$REDIS_KEY" >> .env
+    fi
+    if [[ -n "$SLACK_TOKEN" ]]; then
+        echo "export SLACK_TOKEN=$SLACK_TOKEN" >> .env
+    fi
+    if [[ -n "$WELCOME_CHANNEL" ]]; then
+        echo "export WELCOME_CHANNEL=$WELCOME_CHANNEL" >> .env
+    fi
+fi
 cd cmd/seedlouds
 go build
 ./seedlouds
